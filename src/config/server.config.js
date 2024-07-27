4292db8d1cc8e038ae149957a02c15b7b956c887
Mohit Agrawal <mohitagrawalhnd2003@gmail.com>
2024-07-27 12:26:55 +0530

    Updated package.json And removed unused file

--- src/config/server.config.js
+++ src/config/server.config.js
@@ -1,8 +1,7 @@
 process.loadEnvFile();
 
 module.exports = {
-  PORT: process.env.PORT || 3000,
+  PORT: process.env.PORT,
   MONGO_URI: process.env.MONGO_URI,
-  NODE_ENV: process.env.NODE_ENV,
   LOGDB_URI: process.env.LOGDB_URI,
 };
